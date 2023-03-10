1dc4513699fee7cae54a723eb37759e6a69be44a
sxbl <litalieeen@gmal.com>
2023-03-10 11:17:15 +0100

    securisation login signup supplementaire

--- Models/Model.php
+++ Models/Model.php
@@ -34,63 +34,71 @@ class Model
     }
 
     public function get_connexion()
-{        
-    $email = $_POST['mail'];
-    $password = $_POST['mdp'];
-    
-    $r = $this->bd->prepare("SELECT * FROM user WHERE mail=:email");
-    $r->bindParam(':email', $email);
-    $r->execute();
-    $user = $r->fetch(PDO::FETCH_OBJ);
-    
-    // Vérifier si l'adresse e-mail existe dans la base de données
-    if (!$user) {
-        // L'adresse e-mail n'existe pas dans la base de données, renvoyer un message d'erreur
-        echo "<script>alert('Cette adresse e-mail n'est pas enregistrée, veuillez vous inscrire !');</script>";
-        return;
-    }
-    
-    // Vérifier si le mot de passe correspond à celui stocké dans la base de données
-    if (!password_verify($password, $user->mdp)) {
-        // Le mot de passe ne correspond pas à celui stocké dans la base de données, renvoyer un message d'erreur
-        echo "<script>alert('Le mot de passe est incorrect !');</script>";
-        return;
-    }
-    return $user;
-}
+    {
+        $email = filter_var($_POST['mail'], FILTER_VALIDATE_EMAIL);
+        $password = filter_var($_POST['mdp'], FILTER_SANITIZE_STRING);
 
-    
+        if (!$email) {
+            // L'adresse e-mail n'est pas valide, renvoyer un message d'erreur
+            echo "<script>alert('L\'adresse e-mail n\'est pas valide');</script>";
+            return;
+        }
+
+        $r = $this->bd->prepare("SELECT * FROM user WHERE mail=:email");
+        $r->bindParam(':email', $email);
+        $r->execute();
+        $user = $r->fetch(PDO::FETCH_OBJ);
+
+        // Vérifier si l'adresse e-mail existe dans la base de données
+        if (!$user) {
+            // L'adresse e-mail n'existe pas dans la base de données, renvoyer un message d'erreur
+            echo "<script>alert('Cette adresse e-mail n\'est pas enregistrée, veuillez vous inscrire !');</script>";
+            return;
+        }
+
+        // Vérifier si le mot de passe correspond à celui stocké dans la base de données
+        if (!password_verify($password, $user->mdp)) {
+            // Le mot de passe ne correspond pas à celui stocké dans la base de données, renvoyer un message d'erreur
+            echo "<script>alert('Le mot de passe est incorrect !');</script>";
+            return;
+        }
+
+        // Démarre la session pour stocker l'ID de l'utilisateur connecté
+        session_start();
+        $_SESSION['user_id'] = $user->id;
+
+        return $user;
+    }
+
     public function get_insert_user()
     {
-    // Récupérer les données du formulaire
-    $firstName = trim($_POST['firstName']);
-    $lastName = trim($_POST['lastName']);
-    $mail = trim($_POST['mail']);
-    $mdp = trim($_POST['mdp']);
+        // Récupérer les données du formulaire
+        $firstName = filter_var(trim($_POST['firstName']), FILTER_SANITIZE_STRING);
+        $lastName = filter_var(trim($_POST['lastName']), FILTER_SANITIZE_STRING);
+        $mail = filter_var(trim($_POST['mail']), FILTER_VALIDATE_EMAIL);
+        $mdp = filter_var(trim($_POST['mdp']), FILTER_SANITIZE_STRING);
 
-    // Vérifier si l'adresse e-mail existe déjà dans la base de données
-    $r = $this->bd->prepare("SELECT COUNT(*) FROM `user` WHERE `mail` = :email");
-    $r->bindParam(':email', $mail);
-    $r->execute();
-    $result = $r->fetchColumn();
-    if ($result > 0) {
-        // L'adresse e-mail existe déjà dans la base de données, renvoyer un message d'erreur
-        echo "<script>alert('Cette adresse e-mail est déjà utilisée.');</script>";
-        return;
-    }
+        // Vérifier si l'adresse e-mail existe déjà dans la base de données
+        $r = $this->bd->prepare("SELECT COUNT(*) FROM `user` WHERE `mail` = :email");
+        $r->bindParam(':email', $mail);
+        $r->execute();
+        $result = $r->fetchColumn();
+        if ($result > 0) {
+            // L'adresse e-mail existe déjà dans la base de données, renvoyer un message d'erreur
+            echo "<script>alert('Cette adresse e-mail est déjà utilisée.');</script>";
+            return;
+        }
 
-    // Hasher le mot de passe
-    $mdp_hash = password_hash($mdp, PASSWORD_DEFAULT);
+        // Hasher le mot de passe
+        $mdp_hash = password_hash($mdp, PASSWORD_DEFAULT);
 
-    // Préparer la requête SQL avec une jointure entre les tables commande et fornisseur
-    $r = $this->bd->prepare("INSERT INTO `user`(`nom`, `prenom`, `mail`, `mdp`) VALUES (:nom,:prenom,:email,:pass)");
-    $r->bindParam(':nom', $firstName);
-    $r->bindParam(':prenom', $lastName);
-    $r->bindParam(':email', $mail);
-    $r->bindParam(':pass', $mdp_hash);
-    // Exécuter la requête
-    $r->execute();
-    
+        // Préparer la requête SQL avec une jointure entre les tables commande et fournisseur
+        $r = $this->bd->prepare("INSERT INTO `user`(`nom`, `prenom`, `mail`, `mdp`) VALUES (:nom,:prenom,:email,:pass)");
+        $r->bindParam(':nom', $firstName);
+        $r->bindParam(':prenom', $lastName);
+        $r->bindParam(':email', $mail);
+        $r->bindParam(':pass', $mdp_hash);
+        // Exécuter la requête
+        $r->execute();
     }
 }
-
